c9997a6f9cc6f43925e2a72cca236a869f0a5bde
Alexei <keilalex@yandex.ru>
2023-10-23 20:11:36 +0700

    git init & task1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число");
+int numFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numSecond = Convert.ToInt32(Console.ReadLine());
+if (numFirst > numSecond) {
+    Console.WriteLine("max = " + numFirst);
+    Console.WriteLine("min = " + numSecond);
+}
+else {
+    Console.WriteLine("max = " + numSecond);
+    Console.WriteLine("min = " + numFirst);
+}
